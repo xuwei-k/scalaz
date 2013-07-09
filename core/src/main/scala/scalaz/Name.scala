e52825eb3d1dbcd259137bb34e5542777b07c460
xuwei-k <6b656e6a69@gmail.com>
2013-07-09 16:28:52 +0900

    clear the `Function0` reference of Need
    inspired by sbt `SafeLazy`
    https://github.com/sbt/sbt/blob/v0.12.4/compile/api/SafeLazy.scala

--- core/src/main/scala/scalaz/Name.scala
+++ core/src/main/scala/scalaz/Name.scala
@@ -6,10 +6,16 @@ sealed abstract class Name[+A] {
 }
 
 /** Call by need */
-sealed abstract class Need[+A] extends Name[A]
+final class Need[+A] private(private[this] var eval: () => A) extends Name[A]{
+  lazy val value: A = {
+    val value0 = eval()
+    eval = null
+    value0
+  }
+}
 
 /** Call by value */
-final case class Value[+A](value: A) extends Need[A]
+final case class Value[+A](value: A) extends Name[A]
 
 object Name { 
   def apply[A](a: => A) = new Name[A] {
@@ -35,12 +41,8 @@ object Name {
 }
 
 object Need {
-  def apply[A](a: => A) = {
-    lazy val value0: A = a
-    new Need[A] {
-      def value = value0
-    }
-  }
+  def apply[A](a: => A) = new Need(() => a)
+
   def unapply[A](x: Need[A]): Option[A] = Some(x.value)
 
   implicit val need = new Monad[Need] with Comonad[Need] with Cobind.FromCojoin[Need] with Distributive[Need] {
