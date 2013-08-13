ca5bfe6b94d54b0ea6fdb413ea6fd456df57131e
xuwei-k <6b656e6a69@gmail.com>
2013-08-13 13:24:19 +0900

    Codensity

--- core/src/main/scala/scalaz/Codensity.scala
+++ core/src/main/scala/scalaz/Codensity.scala
@@ -15,11 +15,29 @@ trait Codensity[F[_], A] { self =>
 }
 
 object Codensity {
-  def rep[F[_], A](f: F[A])(implicit F: Monad[F]): Codensity[F, A] =
+  def rep[F[_], A](f: F[A])(implicit F: Bind[F]): Codensity[F, A] =
     new Codensity[F, A] {
       def apply[B](k: A => F[B]) = F.bind(f)(k)
     }
   def pureCodensity[F[_], A](a: => A): Codensity[F, A] = new Codensity[F, A] {
     def apply[B](f: A => F[B]): F[B] = f(a)
   }
+
+  implicit def codensityInstance[F[_]]: Monad[({type λ[α]=Codensity[F, α]})#λ] =
+    new Monad[({type λ[α]=Codensity[F, α]})#λ] {
+      def point[A](a: => A) =
+        pureCodensity(a)
+
+      def bind[A, B](fa: Codensity[F, A])(f: A => Codensity[F, B]) =
+        fa flatMap f
+    }
+
+  implicit val codensityMoandTrans: MonadTrans[Codensity] =
+    new MonadTrans[Codensity] {
+      implicit def apply[G[_]: Monad] =
+        codensityInstance[G]
+
+      def liftM[G[_]: Monad, A](a: G[A]) =
+        rep(a)
+    }
 }
