403f2f1793a8c697b4be32c18e7b217ae5dde7bd
xuwei-k <6b656e6a69@gmail.com>
2013-12-31 15:08:52 +0900

    fix 2.9.3 build

--- core/src/main/scala/scalaz/std/math/BigDecimal.scala
+++ core/src/main/scala/scalaz/std/math/BigDecimal.scala
@@ -3,21 +3,22 @@ package std
 package math
 
 import _root_.java.math.MathContext.UNLIMITED
+import _root_.java.math.{BigDecimal => JavaBigDecimal}
 
 trait BigDecimalInstances {
   implicit val bigDecimalInstance: Monoid[BigDecimal] with Enum[BigDecimal] with Show[BigDecimal] = new Monoid[BigDecimal] with Enum[BigDecimal] with Show[BigDecimal] {
     override def shows(f: BigDecimal) = f.toString
 
-    def append(f1: BigDecimal, f2: => BigDecimal) = f1 + f2
+    def append(f1: BigDecimal, f2: => BigDecimal) = f1.bigDecimal.add(f2.bigDecimal, UNLIMITED)
 
     def zero = 0L
 
     def order(x: BigDecimal, y: BigDecimal) = if (x < y) Ordering.LT else if (x == y) Ordering.EQ else Ordering.GT
 
-    def succ(b: BigDecimal) = b + 1
-    def pred(b: BigDecimal) = b - 1
-    override def succn(a: Int, b: BigDecimal) = b + a
-    override def predn(a: Int, b: BigDecimal) = b - a
+    def succ(b: BigDecimal) = b.bigDecimal.add(new JavaBigDecimal(1), UNLIMITED)
+    def pred(b: BigDecimal) = b.bigDecimal.subtract(new JavaBigDecimal(1), UNLIMITED)
+    override def succn(a: Int, b: BigDecimal) = b.bigDecimal.add(new JavaBigDecimal(a), UNLIMITED)
+    override def predn(a: Int, b: BigDecimal) = b.bigDecimal.subtract(new JavaBigDecimal(a), UNLIMITED)
     override def min = None
     override def max = None
   }
