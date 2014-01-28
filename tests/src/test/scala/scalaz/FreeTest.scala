4b84fcb6b501edb11b4667bc6642bda52b580ad5
xuwei-k <6b656e6a69@gmail.com>
2014-01-29 05:53:51 +0900

    FreeTest

--- tests/src/test/scala/scalaz/FreeTest.scala
+++ tests/src/test/scala/scalaz/FreeTest.scala
@@ -1,50 +1,50 @@
 package scalaz
 
+import org.scalacheck.{Arbitrary, Gen}
 import Isomorphism._
 import std.AllInstances._
 import Free._
+import scalaz.scalacheck.ScalazProperties._
+import scalaz.scalacheck.ScalaCheckBinding._
 
-object FreeTest extends SpecLite {
+object FreeTest1 extends SpecLite {
 
-  type PairOpt[α] = Option[(α, α)]
-  type FList[A] = Free[PairOpt, A]
+  type PairOpt[A] = Option[(A, A)]
+  type BinaryTree[A] = Free[PairOpt, A]
 
-  implicit val pairOptInstance: Functor[PairOpt] with Foldable[PairOpt] =
-    new Functor[PairOpt] with Foldable.FromFoldr[PairOpt]{
-      def map[A, B](fa: PairOpt[A])(f: A => B) =
-        fa.map(t => (f(t._1), f(t._2)))
-
-      def foldRight[A, B](fa: PairOpt[A], z: => B)(f: (A, => B) => B) =
-        fa.map(t => f(t._1, f(t._2, z))).getOrElse(z)
+  implicit val pairOptInstance: Traverse[PairOpt] =
+    new Traverse[PairOpt] {
+      override def map[A, B](fa: PairOpt[A])(f: A => B) =
+        fa.map{case (a, b) => (f(a), f(b))}
+      def traverseImpl[G[_], A, B](fa: PairOpt[A])(f: A => G[B])(implicit G: Applicative[G]) =
+        Traverse[Option].traverse(fa){case (a, b) => G.tuple2(f(a), f(b))}
     }
 
-  object FList{
-    private[this] val _nil = Suspend[PairOpt, Nothing](None)
+  implicit val pairOptArb: Arbitrary ~> ({type λ[α] = Arbitrary[PairOpt[α]]})#λ =
+    new (Arbitrary ~> ({type λ[α] = Arbitrary[PairOpt[α]]})#λ){
+      def apply[A](a: Arbitrary[A]) = {
+        implicit val a0 = a
+        implicitly[Arbitrary[PairOpt[A]]]
+      }
+    }
 
-    def nil[A]: FList[A] = _nil.asInstanceOf[FList[A]]
+  implicit def freeArb[F[_], A](implicit A: Arbitrary[A], F: Arbitrary ~> ({type λ[α] = Arbitrary[F[α]]})#λ): Arbitrary[Free[F, A]] =
+    Arbitrary(Gen.oneOf(
+      Functor[Arbitrary].map(A)(Return[F, A](_)).arbitrary,
+      Functor[Arbitrary].map(F(freeArb[F, A]))(Suspend[F, A](_)).arbitrary
+    ))
 
-    def cons[A](head: A, tail: FList[A]): FList[A] =
-      Suspend[PairOpt, A](Option(Return[PairOpt, A](head) -> tail))
-
-    def apply[A](xs: A*): FList[A] = listFListIso.from(xs.toList)
+  implicit val binaryTreeEq = new (Equal ~> ({type λ[α] = Equal[PairOpt[α]]})#λ){
+    def apply[A](a: Equal[A]) = {
+      implicit val a0 = a
+      Equal[PairOpt[A]]
+    }
   }
 
-  val listFListIso: FList <~> List =
-    new IsoFunctorTemplate[FList, List]{
-      def to[A](ga: FList[A]) = {
-        Foldable[FList].foldRight(ga, List[A]())(_ :: _)
-      }
+  checkAll(traverse.laws[PairOpt])
+  checkAll(equal.laws[PairOpt[Int]])
 
-      def from[A](fa: List[A]) =
-        fa.foldRight(FList.nil[A])(FList.cons[A])
-    }
-
-  implicit def FListInstance0[A: Equal]: Equal[FList[A]] =
-    new IsomorphismEqual[FList[A], List[A]]{
-      def G = Equal[List[A]]
-      def iso = listFListIso.unlift[A]
-    }
-
+  checkAll(traverse.laws[BinaryTree])
+  checkAll(equal.laws[BinaryTree[Int]])
 }
-
 
