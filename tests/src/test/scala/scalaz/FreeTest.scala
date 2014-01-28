84a98a8c2f9a38cc77d1a6a74a229a251455a24a
xuwei-k <6b656e6a69@gmail.com>
2014-01-29 06:23:23 +0900

    FreeTest

--- tests/src/test/scala/scalaz/FreeTest.scala
+++ tests/src/test/scala/scalaz/FreeTest.scala
@@ -3,30 +3,12 @@ package scalaz
 import org.scalacheck.{Arbitrary, Gen}
 import Isomorphism._
 import std.AllInstances._
-import Free._
+import Free.{Return, Suspend}
 import scalaz.scalacheck.ScalazProperties._
 import scalaz.scalacheck.ScalaCheckBinding._
+import scalaz.scalacheck.ScalazArbitrary._
 
-object FreeTest1 extends SpecLite {
-
-  type PairOpt[A] = Option[(A, A)]
-  type BinaryTree[A] = Free[PairOpt, A]
-
-  implicit val pairOptInstance: Traverse[PairOpt] =
-    new Traverse[PairOpt] {
-      override def map[A, B](fa: PairOpt[A])(f: A => B) =
-        fa.map{case (a, b) => (f(a), f(b))}
-      def traverseImpl[G[_], A, B](fa: PairOpt[A])(f: A => G[B])(implicit G: Applicative[G]) =
-        Traverse[Option].traverse(fa){case (a, b) => G.tuple2(f(a), f(b))}
-    }
-
-  implicit val pairOptArb: Arbitrary ~> ({type λ[α] = Arbitrary[PairOpt[α]]})#λ =
-    new (Arbitrary ~> ({type λ[α] = Arbitrary[PairOpt[α]]})#λ){
-      def apply[A](a: Arbitrary[A]) = {
-        implicit val a0 = a
-        implicitly[Arbitrary[PairOpt[A]]]
-      }
-    }
+object FreeTest extends SpecLite {
 
   implicit def freeArb[F[_], A](implicit A: Arbitrary[A], F: Arbitrary ~> ({type λ[α] = Arbitrary[F[α]]})#λ): Arbitrary[Free[F, A]] =
     Arbitrary(Gen.oneOf(
@@ -34,17 +16,54 @@ object FreeTest1 extends SpecLite {
       Functor[Arbitrary].map(F(freeArb[F, A]))(Suspend[F, A](_)).arbitrary
     ))
 
-  implicit val binaryTreeEq = new (Equal ~> ({type λ[α] = Equal[PairOpt[α]]})#λ){
-    def apply[A](a: Equal[A]) = {
-      implicit val a0 = a
-      Equal[PairOpt[A]]
-    }
+  private trait Template[F[_], G[_]] extends (G ~> ({type λ[α] = G[F[α]]})#λ) {
+    override final def apply[A](a: G[A]) = lift(a)
+
+    def lift[A: G]: G[F[A]]
   }
 
-  checkAll(traverse.laws[PairOpt])
-  checkAll(equal.laws[PairOpt[Int]])
+  "Option[(A, A)]" should {
+    type PairOpt[A] = Option[(A, A)]
+    type BinaryTree[A] = Free[PairOpt, A]
 
-  checkAll(traverse.laws[BinaryTree])
-  checkAll(equal.laws[BinaryTree[Int]])
+    implicit val pairOptInstance: Traverse[PairOpt] =
+      new Traverse[PairOpt] {
+        override def map[A, B](fa: PairOpt[A])(f: A => B) =
+          fa.map{case (a, b) => (f(a), f(b))}
+        def traverseImpl[G[_], A, B](fa: PairOpt[A])(f: A => G[B])(implicit G: Applicative[G]) =
+          Traverse[Option].traverse(fa){case (a, b) => G.tuple2(f(a), f(b))}
+      }
+
+    implicit val pairOptArb = new Template[PairOpt, Arbitrary] {
+      def lift[A: Arbitrary] = implicitly[Arbitrary[PairOpt[A]]]
+    }
+
+    implicit val binaryTreeEq = new Template[PairOpt, Equal] {
+      def lift[A: Equal] = Equal[PairOpt[A]]
+    }
+
+    checkAll(traverse.laws[PairOpt])
+    checkAll(equal.laws[PairOpt[Int]])
+
+    checkAll(traverse.laws[BinaryTree])
+    checkAll(equal.laws[BinaryTree[Int]])
+  }
+
+  "OneAnd[Option, A]" should {
+    type OneAndOpt[A] = OneAnd[Option, A]
+    type FreeOneAndOpt[A] = Free[OneAndOpt, A]
+
+    implicit val oneAndOptArb = new Template[OneAndOpt, Arbitrary] {
+      def lift[A: Arbitrary] = implicitly[Arbitrary[OneAndOpt[A]]]
+    }
+
+    implicit val oneAndOptEqual = new Template[OneAndOpt, Equal] {
+      def lift[A: Equal] = Equal[OneAndOpt[A]]
+    }
+
+    checkAll(traverse1.laws[FreeOneAndOpt])
+    checkAll(equal.laws[FreeOneAndOpt[Int]])
+  }
+
 }
 
