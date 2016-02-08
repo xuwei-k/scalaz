963ab9dc953465b1949137111a8545395087294e
xuwei-k <6b656e6a69@gmail.com>
2016-02-09 04:14:35 +0900

    MonoidCoproductTest

--- tests/src/test/scala/scalaz/MonoidCoproductTest.scala
+++ tests/src/test/scala/scalaz/MonoidCoproductTest.scala
@@ -1,12 +1,10 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
-import std.anyVal._, std.string._
+import std.anyVal._
 
-object MonoidCoproductTest extends SpecLite {
-
-  checkAll(monoid.laws[Int :+: String])
-  checkAll(equal.laws[Int :+: String])
-
+object MonoidCoproductTest extends Scalaprops {
+  val testLaws = Properties.list(
+    laws.monoid.all[Int :+: Byte],
+    laws.equal.all[Int :+: Byte]
+  )
 }
