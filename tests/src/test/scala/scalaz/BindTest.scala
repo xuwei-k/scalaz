597a64cb6fdc799576201b53ca3d0cd10581eb13
xuwei-k <6b656e6a69@gmail.com>
2016-02-09 04:35:57 +0900

    BindTest

--- tests/src/test/scala/scalaz/BindTest.scala
+++ tests/src/test/scala/scalaz/BindTest.scala
@@ -2,22 +2,23 @@ package scalaz
 
 import std.AllInstances._
 import syntax.bind._
+import Property.forAll
 
-object BindTest extends SpecLite {
+object BindTest extends Scalaprops {
 
-  ">>=" in {
+  val >>= = forAll {
     (List(1, 2, 3) >>= (x => List(x, x))) must_===(List(1, 1, 2, 2, 3, 3))
   }
 
-  "ifM" in {
+  val ifM = forAll {
     List(true, false, true).ifM(List(0), List(1, 1)) must_===(List(0, 1, 1, 0))
   }
 
-  ">>" in {
+  val >> = forAll {
     List(1, 2, 3) >> (List(2, 3)) must_===(List(2, 3, 2, 3, 2, 3))
   }
 
-  "join" in {
+  val join = forAll {
     List(List(1, 2)).join must_===(List(1, 2))
   }
 }
