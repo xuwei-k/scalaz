31cc635f61bcc6c2bf579745fb4e8821a332b144
xuwei-k <6b656e6a69@gmail.com>
2016-02-10 02:45:39 +0900

    SyntaxTest

--- tests/src/test/scala/scalaz/SyntaxTest.scala
+++ tests/src/test/scala/scalaz/SyntaxTest.scala
@@ -1,20 +1,22 @@
 package scalaz
 
-object SyntaxTest extends SpecLite {
-  "functor syntax" in {
+import Property.forAll
+
+object SyntaxTest extends Scalaprops {
+  val `functor syntax` = forAll{
     import syntax.functor._
     import std.tuple._, std.anyVal._
 
     ((1, 2) ∘ (1 +) ∘ (1 +)) must_===((1, 4))
   }
 
-  "functor syntax missing imports 1" in {
+  val `functor syntax missing imports 1` = forAll{
     import syntax.functor._
     //List(1) ∘ (1 +) // uncomment to see the type error for missing type class instances
     ()
   }
 
-  "functor syntax missing imports 2" in {
+  val `functor syntax missing imports 2` = forAll{
     import syntax.functor._
     //(1, 2) ∘ (1 +) // uncomment to see the type error for missing type class instances, based on the @implicitNotFound annotation on scalaz.Unapply.
     ()
