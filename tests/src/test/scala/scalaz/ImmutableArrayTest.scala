9e20ec47965637dc9f33ed5e5ea0a7811255ab7c
xuwei-k <6b656e6a69@gmail.com>
2016-02-09 10:24:06 +0900

    ImmutableArrayTest

--- tests/src/test/scala/scalaz/ImmutableArrayTest.scala
+++ tests/src/test/scala/scalaz/ImmutableArrayTest.scala
@@ -1,23 +1,25 @@
 package scalaz
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
+
 import std.anyVal._
+import Property.forAll
 
-object ImmutableArrayTest extends SpecLite {
+object ImmutableArrayTest extends Scalaprops {
 
-  "Issue #525" in {
+  val `Issue #525` = forAll {
     val xs = ImmutableArray.fromArray(Array(1)) ++ ImmutableArray.fromArray(Array("a"))
     xs.toArray.toList must_==(Array(1, "a").toList)
   }
 
-  "Issue #812" in {
+  val `Issue #812` = forAll {
     val xs = ImmutableArray.fromArray(Array("test"))
     val t = xs.tail
     t.toArray.toList must_==(Array().toList)
   }
 
-  checkAll(equal.laws[ImmutableArray[Int]])
-  checkAll(foldable.laws[ImmutableArray])
-  checkAll(FoldableTests.anyAndAllLazy[ImmutableArray])
+  val testLaws = Properties.list(
+    laws.equal.all[ImmutableArray[Int]],
+    laws.foldable.all[ImmutableArray],
+    laws.foldable.anyAndAllLazy[ImmutableArray]
+  )
 
 }
