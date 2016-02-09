97b5689f000fdf85ef192bd8767468dacc1889b3
xuwei-k <6b656e6a69@gmail.com>
2016-02-09 10:21:26 +0900

    EndoTest

--- tests/src/test/scala/scalaz/EndoTest.scala
+++ tests/src/test/scala/scalaz/EndoTest.scala
@@ -1,19 +1,12 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalaCheckBinding._
-import org.scalacheck.Arbitrary
-
-object EndoTest extends SpecLite {
-
-  implicit def endoArb[A](implicit A: Arbitrary[A => A]): Arbitrary[Endo[A]] =
-    Functor[Arbitrary].map(A)(Endo.endo)
+object EndoTest extends Scalaprops {
 
   implicit val endoIntEqual: Equal[Endo[Int]] =
     Equal.equal( (a, b) =>
       Iterator.fill(20)(util.Random.nextInt).forall(n => a(n) == b(n))
     )
 
-  checkAll(invariantFunctor.laws[Endo])
+  val testLaws = laws.invariantFunctor.all[Endo]
 
 }
