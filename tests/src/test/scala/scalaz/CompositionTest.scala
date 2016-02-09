fb79679cf95dff72707997abfa311363a50edeb4
xuwei-k <6b656e6a69@gmail.com>
2016-02-10 03:09:31 +0900

    CompositionTest

--- tests/src/test/scala/scalaz/CompositionTest.scala
+++ tests/src/test/scala/scalaz/CompositionTest.scala
@@ -1,24 +1,24 @@
 package scalaz
 
 import std.AllInstances._
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
 
-object CompositionTest extends SpecLite {
+object CompositionTest extends Scalaprops {
   type OptionList[α] = Option[List[α]]
 
   implicit val optionListApplicative = ApplicativePlus[Option].compose[List]
   implicit val optionListTraverse = Traverse[Option].compose[List]
   implicit val oneAndOptNelTraverse = Traverse1[OneAnd[Option, ?]].compose[NonEmptyList]
 
-  checkAll(applicative.laws[OptionList])
-  checkAll(plusEmpty.laws[OptionList])
-  checkAll(traverse.laws[OptionList])
-  checkAll(traverse1.laws[λ[α => OneAnd[Option, NonEmptyList[α]]]])
+  val optionList = Properties.list(
+    laws.applicativePlus.all[OptionList],
+    laws.traverse.all[OptionList]
+  )
+
+  val traverse1 = laws.traverse1.all[λ[α => OneAnd[Option, NonEmptyList[α]]]]
 
   implicit val eitherTuple2 = Bitraverse[Either].compose[Tuple2]
-  checkAll(bitraverse.laws[λ[(α, β) => Either[(α, β), (α, β)]]])
+  val eitherTuple2Test = laws.bitraverse.all[λ[(α, β) => Either[(α, β), (α, β)]]]
 
   implicit val listEitherBitraverse = Traverse[List].bicompose[Either]
-  checkAll(bitraverse.laws[λ[(α, β) => List[Either[α, β]]]])
+  val listEitherTest = laws.bitraverse.all[λ[(α, β) => List[Either[α, β]]]]
 }
