4cb2ae70570c6d89e549762d8ab675cb29698dde
xuwei-k <6b656e6a69@gmail.com>
2016-02-11 02:56:05 +0900

    add FunctionEqual

---
+++ tests/src/test/scala/scalaz/FunctionEqual.scala
@@ -0,0 +1,22 @@
+package scalaz
+
+import scala.util.Random
+import scalaz._
+
+object FunctionEqual extends FunctionEqual(10)
+
+sealed class FunctionEqual(size: Int) {
+  implicit def f1[A1: Gen, B](implicit B: Equal[B]): Equal[A1 => B] = {
+    val values = Gen[A1].samples(listSize = size, size = size, seed = Random.nextLong())
+
+    Equal.equal((x, y) =>
+      values.forall(a => B.equal(x(a), y(a)))
+    )
+  }
+
+  implicit def f2[A1: Gen, A2: Gen, B](implicit B: Equal[B]): Equal[(A1, A2) => B] =
+    f1[(A1, A2), B].contramap(_.tupled)
+
+  implicit def f3[A1: Gen, A2: Gen, A3: Gen, B](implicit B: Equal[B]): Equal[(A1, A2, A3) => B] =
+    f1[(A1, A2, A3), B].contramap(_.tupled)
+}
