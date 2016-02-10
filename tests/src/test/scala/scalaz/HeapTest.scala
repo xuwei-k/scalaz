e69b5140cdbb1e527a6f4cba96b0414087114b99
xuwei-k <6b656e6a69@gmail.com>
2016-02-11 02:27:46 +0900

    HeapTest

--- tests/src/test/scala/scalaz/HeapTest.scala
+++ tests/src/test/scala/scalaz/HeapTest.scala
@@ -1,37 +1,34 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
 import scalaz.std.AllInstances._
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object HeapTest extends SpecLite {
-  checkAll(equal.laws[Heap[Int]])
-  checkAll(monoid.laws[Heap[Int]])
-  checkAll(foldable.laws[Heap])
+object HeapTest extends Scalaprops {
+  val equal = laws.equal.all[Heap[Int]]
+  val monoid = laws.monoid.all[Heap[Int]]
+  val foldable = laws.foldable.all[Heap]
 
-  def pred(i: Int) = i % 2 == 0
-
-  "order maintained for toList" ! forAll {
+  val `order maintained for toList` = forAll {
     (a: Heap[Int]) => a.toList must_===(a.toList.sorted)
   }
 
-  "toList / toStream" ! forAll {
+  val `toList / toStream` = forAll {
     (a: Heap[Int]) => a.toStream must_===(a.toList.toStream)
   }
 
-  "filter" ! forAll {
-    (a: Heap[Int]) => a.filter(pred).toStream must_===(a.toStream.filter(pred))
+  val filter = forAll {
+    (a: Heap[Int], pred: Int => Boolean) =>
+      a.filter(pred).toStream must_===(a.toStream.filter(pred))
   }
 
-  "partition" ! forAll {
-    (a: Heap[Int]) =>
+  val partition = forAll {
+    (a: Heap[Int], pred: Int => Boolean) =>
       val (ts, fs) = a.partition(pred)
       ts.forall(pred) must_===(true)
       fs.exists(pred) must_===(false)
   }
 
-  "split" ! forAll {
+  val split = forAll {
     (a: Heap[Int], x: Int) =>
       val (lt, eq, gt) = a.split(x)
       lt.forall(_ < x) must_===(true)
