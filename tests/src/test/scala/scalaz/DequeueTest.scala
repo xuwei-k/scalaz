bc5961a100191149a7fb81ca9a2033ca1ee68aa3
xuwei-k <6b656e6a69@gmail.com>
2016-02-13 11:13:10 +0900

    DequeueTest

--- tests/src/test/scala/scalaz/DequeueTest.scala
+++ tests/src/test/scala/scalaz/DequeueTest.scala
@@ -1,46 +1,47 @@
 package scalaz
 
 import std.AllInstances._
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object DequeueTest extends SpecLite {
-  checkAll(monoid.laws[Dequeue[Int]])
-  checkAll(isEmpty.laws[Dequeue])
-  checkAll(foldable.laws[Dequeue])
-  checkAll(plusEmpty.laws[Dequeue])
-  checkAll(functor.laws[Dequeue])
+object DequeueTest extends Scalaprops {
 
-  "fromList works" ! forAll{ (l: List[Int]) ⇒
+  val testLaws = Properties.list(
+    laws.monoid.all[Dequeue[Int]],
+    laws.isEmpty.all[Dequeue],
+    laws.foldable.all[Dequeue],
+    laws.plusEmpty.all[Dequeue],
+    laws.functor.all[Dequeue]
+  )
+
+  val `fromList works` = forAll{ (l: List[Int]) ⇒
     Dequeue.fromFoldable(l).toStream must_===(l.toStream)
   }
 
-  "reverse twice is id" ! forAll{ (dq: Dequeue[Int]) ⇒
+  val `reverse twice is id` = forAll{ (dq: Dequeue[Int]) ⇒
     dq.reverse.reverse.toIList.must_===(dq.toIList)
   }
 
-  "size consistent with source list" ! forAll{ (l: List[Int]) ⇒
+  val `size consistent with source list` = forAll{ (l: List[Int]) ⇒
     Dequeue.fromFoldable(l).size must_===(l.size)
   }
 
-  "reverse stream works" ! forAll{ (l: List[Int]) ⇒
+  val `reverse stream works` = forAll{ (l: List[Int]) ⇒
     Dequeue.fromFoldable(l).toBackStream must_===(l.reverse.toStream)
   }
 
-  "toIList consistent with Ilist.fromFoldable" ! forAll{ (l: List[Int]) ⇒
+  val `toIList consistent with Ilist.fromFoldable` = forAll{ (l: List[Int]) ⇒
     Dequeue.fromFoldable(l).toIList must_===(IList.fromFoldable(l))
   }
 
-  "toBackIList consistent with Ilist.fromFoldable" ! forAll{ (l: List[Int]) ⇒
+  val `toBackIList consistent with Ilist.fromFoldable` = forAll{ (l: List[Int]) ⇒
     Dequeue.fromFoldable(l).toBackIList must_===(IList.fromFoldable(l.reverse))
   }
 
-  "snoc works" ! forAll{ (l: List[Int]) ⇒
+  val `snoc works` = forAll{ (l: List[Int]) ⇒
     (l.foldLeft[Dequeue[Int]](Dequeue.empty)((q,a) ⇒ q snoc a)).toStream must_=== l.toStream
   }
 
-  "cons works" ! forAll{ (l: List[Int]) ⇒
+  val `cons works` = forAll{ (l: List[Int]) ⇒
     (l.foldRight[Dequeue[Int]](Dequeue.empty)((a,q) ⇒ q cons a)).toStream must_=== l.toStream
   }
 }
