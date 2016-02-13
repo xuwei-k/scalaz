12472c2add5c66f82e68fed4bcf3ffd3f3aa8e91
xuwei-k <6b656e6a69@gmail.com>
2016-02-14 00:52:31 +0900

    CordTest

--- tests/src/test/scala/scalaz/CordTest.scala
+++ tests/src/test/scala/scalaz/CordTest.scala
@@ -1,13 +1,12 @@
 package scalaz
 
-import org.scalacheck.Arbitrary
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalaCheckBinding._
-import Cord._
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object CordTest extends SpecLite {
-  "split() must result in two cords whose summary length is equal to the length of original cord " in {
+object CordTest extends Scalaprops {
+  private[this] implicit val gen =
+    Tag.unsubst(Gen[String @@ GenTags.AlphaNum]).map(Cord.stringToCord)
+
+  val `split() must result in two cords whose summary length is equal to the length of original cord` = forAll {
     val x = Cord("Once upon a midnight dreary")
     for (i <- 0 until x.length) {
       val split = x split i
@@ -15,7 +14,7 @@ object CordTest extends SpecLite {
     }
   }
 
-  "drop() must make cord shorter" in {
+  val `drop() must make cord shorter` = forAll {
     val theString = "While I pondered, weak and weary"
     val x = Cord(theString)
     for (i <- 0 until x.length) {
@@ -24,28 +23,28 @@ object CordTest extends SpecLite {
     }
   }
 
-  "tail() must be smaller than the whole, generally" in {
+  val `tail() must be smaller than the whole, generally` = forAll {
     val x = Cord("abc")
     x.tail.toString must_== "bc"
     x.tail.tail.toString must_== "c"
     x.tail.tail.tail.toString must_== ""
   }
 
-  "isEmpty() must indicate string is empty" ! forAll { (a:Cord, b:Cord) =>
+  val `isEmpty() must indicate string is empty` = forAll { (a:Cord, b:Cord) =>
     a.isEmpty == a.toString.isEmpty &&
     b.isEmpty == b.toString.isEmpty &&
     (a ++ b).isEmpty == (a.toString.isEmpty && b.toString.isEmpty)
   }
 
-  "nonEmpty() must indicate string is non-empty" ! forAll { (a:Cord, b:Cord) =>
+  val `nonEmpty() must indicate string is non-empty` = forAll { (a:Cord, b:Cord) =>
     val c = a ++ b
     a.nonEmpty == !a.toString.isEmpty &&
     b.nonEmpty == !b.toString.isEmpty &&
     (a ++ b).nonEmpty == (!a.toString.isEmpty || !b.toString.isEmpty)
   }
 
-  implicit def ArbitraryCord: Arbitrary[Cord] = Functor[Arbitrary].map(implicitly[Arbitrary[String]])(Cord.stringToCord)
-
-  checkAll(monoid.laws[Cord])
-  checkAll(equal.laws[Cord])
+  val testLaws = Properties.list(
+    laws.monoid.all[Cord],
+    laws.equal.all[Cord]
+  )
 }
