2e164015151badb205c765635d6e0b0bb4251307
xuwei-k <6b656e6a69@gmail.com>
2016-02-14 00:47:07 +0900

    DievTest

--- tests/src/test/scala/scalaz/DievTest.scala
+++ tests/src/test/scala/scalaz/DievTest.scala
@@ -1,15 +1,13 @@
 package scalaz
 
 import std.AllInstances._
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
 import scala.util.Random
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object DievTest extends SpecLite {
+object DievTest extends Scalaprops {
   val random = new Random()
 
-  "insert order makes no difference" ! forAll {
+  val `insert order makes no difference` = forAll {
     (list: List[Int]) => {
       val shuffledList = random.shuffle(list)
       val dievFromList = list.foldLeft(Diev.empty[Int])(_ + _)
@@ -18,38 +16,38 @@ object DievTest extends SpecLite {
     }
   }
 
-  "fixIntervalOrder" ! forAll {
+  val fixIntervalOrder = forAll {
     (tuple: (Int, Int)) => {
       val expectedResult = if (tuple._1 > tuple._2) tuple.swap else tuple
       DievInterval.fixIntervalOrder(tuple) must_===(expectedResult)
     }
   }
 
-  // TODO: Use data table to test subtractInterval.
-
-  "fromValuesSeq / toSet" ! forAll {
+  val `fromValuesSeq / toSet` = forAll {
     (set: Set[Int]) => Diev.fromValuesSeq(set.toSeq).toSet must_===(set)
   }
 
-  "fromValuesSeq / toList" ! forAll {
+  val `fromValuesSeq / toList` = forAll {
     (list: List[Int]) => {
       val sortedList = list.toSet.toList.sorted
       Diev.fromValuesSeq(list).toList must_===(sortedList)
     }
   }
 
-  "++ associativity" ! forAll {
+  val `++ associativity` = forAll {
     (first: Diev[Int], second: Diev[Int]) => first ++ second must_===(second ++ first)
   }
 
-  "intervals / fromIntervalsSeq" ! forAll {
+  val `intervals / fromIntervalsSeq` = forAll {
     (original: Diev[Int]) => Diev.fromIntervalsSeq(original.intervals) must_===(original)
   }
 
-  "-- / ++" ! forAll {
+  val `-- / ++` = forAll {
     (first: Diev[Int], second: Diev[Int]) => first -- second ++ second must_===(first ++ second)
   }
 
-  checkAll(equal.laws[Diev[Int]])
-  checkAll(monoid.laws[Diev[Int]])
+  val testLaws = Properties.list(
+    laws.equal.all[Diev[Int]],
+    laws.monoid.all[Diev[Int]]
+  )
 }
