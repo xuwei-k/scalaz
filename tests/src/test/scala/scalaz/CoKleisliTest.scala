1e3e9ada91b670fca46bae18fbc7d5073149dad5
xuwei-k <6b656e6a69@gmail.com>
2016-02-14 00:37:12 +0900

    CoKleisliTest, EndomorphicTest

--- tests/src/test/scala/scalaz/CoKleisliTest.scala
+++ tests/src/test/scala/scalaz/CoKleisliTest.scala
@@ -1,48 +1,20 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import org.scalacheck.Arbitrary
+import Property.forAll
 
-object CokleisliTest extends SpecLite {
-
-  implicit val cokleisliArb1: Arbitrary[Cokleisli[Option, Int, Int]] = {
-    def arb(f: (Option[Int], Int, Int) => Int): org.scalacheck.Gen[Cokleisli[Option, Int, Int]] =
-      for{
-        a <- implicitly[Arbitrary[Int]].arbitrary
-        b <- implicitly[Arbitrary[Int]].arbitrary
-      } yield Cokleisli[Option, Int, Int](f(_, a, b))
-
-    Arbitrary(org.scalacheck.Gen.oneOf(
-      arb((_, n, _) => n),
-      arb((a, b, c) => a getOrElse b),
-      arb((a, b, c) => a.map(_ + b) getOrElse c),
-      arb((a, b, c) => a.map(_ - b) getOrElse c)
-    ))
-  }
-
-  implicit val cokleisliArb2: Arbitrary[Cokleisli[Option, Int, Int => Int]] = {
-    def arb(f: (Option[Int], Int) => (Int => Int)): org.scalacheck.Gen[Cokleisli[Option, Int, Int => Int]] =
-      implicitly[Arbitrary[Int]].arbitrary.map(a => Cokleisli[Option, Int, Int => Int](f(_, a)))
-
-    Arbitrary(org.scalacheck.Gen.oneOf(
-      arb((_, n) => Function.const(n)),
-      arb((_, _) => x => x),
-      arb((_, n) => _ + n),
-      arb((_, n) => _ - n),
-      arb((a, b) => a.map(_ + b) getOrElse _),
-      arb((a, b) => a.map(_ - b) getOrElse _)
-    ))
-  }
+object CokleisliTest extends Scalaprops {
 
   implicit val cokleisliEqual: Equal[Cokleisli[Option, Int, Int]] =
     Equal.equal{ (a, b) =>
       a(None) == b(None) && Iterator.fill(20)(util.Random.nextInt).map(Option(_)).forall(n => a(n) == b(n))
     }
 
-  checkAll(bindRec.laws[Cokleisli[Option, Int, ?]])
-  checkAll(bind.laws[Cokleisli[Option, Int, ?]])
+  val testLaws = Properties.list(
+    laws.bindRec.all[Cokleisli[Option, Int, ?]],
+    laws.monad.all[Cokleisli[Option, Int, ?]]
+  )
 
-  "compose" in {
+  val compose = forAll {
     import std.AllInstances._
 
     val ck = Cokleisli((a: NonEmptyList[Int]) => a.size)
--- tests/src/test/scala/scalaz/EndomorphicTest.scala
+++ tests/src/test/scala/scalaz/EndomorphicTest.scala
@@ -1,27 +1,18 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalaCheckBinding._
-import scalaz.scalacheck.ScalazArbitrary._
-import org.scalacheck.Arbitrary
+import std.AllInstances._
 import KleisliTest._
 import CokleisliTest._
-import std.AllInstances._
 
-object EndomorphicTest extends SpecLite {
+object EndomorphicTest extends Scalaprops {
 
-  implicit def endoEqual[F[_], G[_[_], _, _], A](
+  private[this] implicit def endoEqual[F[_], G[_[_], _, _], A](
     implicit F: Equal[G[F, A, A]]
   ): Equal[Endomorphic[G[F, ?, ?], A]] =
     Equal.equalBy(_.run)
 
-  implicit def endoArb[F[_], G[_[_], _, _], A](
-    implicit F: Arbitrary[G[F, A, A]]
-  ): Arbitrary[Endomorphic[G[F, ?, ?], A]] =
-    Functor[Arbitrary].map(F)(Endomorphic[G[F, ?, ?], A](_))
-
-  checkAll(monoid.laws[Endomorphic[Kleisli[Option, ?, ?], Int]])
-  checkAll(semigroup.laws[Endomorphic[Cokleisli[Option, ?, ?], Int]])
+  val kleisliOption = laws.monoid.all[Endomorphic[Kleisli[Option, ?, ?], Int]]
+  val cokleisliOption = laws.semigroup.all[Endomorphic[Cokleisli[Option, ?, ?], Int]]
 
   object instances{
     def semigroup[F[_, _]: Compose, A] = Semigroup[Endomorphic[F, A]]
