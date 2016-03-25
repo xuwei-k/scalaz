6fa0ad5bd0189fe6fb3cd7e2c598cb45ce696cd2
xuwei-k <6b656e6a69@gmail.com>
2016-03-26 05:25:03 +0900

    AlignTest

--- tests/src/test/scala/scalaz/AlignTest.scala
+++ tests/src/test/scala/scalaz/AlignTest.scala
@@ -1,13 +1,13 @@
 package scalaz
 
 import std.AllInstances._
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object AlignTest extends SpecLite {
+object AlignTest extends Scalaprops {
 
   val F = Align[List]
 
-  "pad" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val pad = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.pad(xs, ys)
     xys.foreach(_. mustMatch {case (None, None) => false; case _ => true})
     val max = xs.size max ys.size
@@ -21,41 +21,41 @@ object AlignTest extends SpecLite {
       dropped.foreach(_ mustMatch {case (None, Some(_)) => true})
   }
 
-  "merge" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val merge = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.merge(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys must_===(xs.zipAll(ys, 0, 0).map{case (x, y) => x + y})
   }
 
-  "alignA" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val alignA = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.alignA(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys.takeWhile(_.isDefined).size must_===(xs.size)
     (xys.dropWhile(_.isDefined)).foreach(_ must_== None)
   }
 
-  "alignB" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val alignB = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.alignB(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys.takeWhile(_.isDefined).size must_===(ys.size)
     xys.dropWhile(_.isDefined).foreach(_ must_== None)
   }
 
-  "alignThis" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val alignThis = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.alignThis(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys.dropWhile(_.isEmpty).size must_===((xs.size - ys.size) max 0)
     xys.dropWhile(_.isEmpty) foreach (_ mustMatch { case Some(_) => true })
   }
 
-  "alignThat" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val alignThat = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.alignThat(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys.dropWhile(_.isEmpty).size must_===((ys.size - xs.size) max 0)
     xys.dropWhile(_.isEmpty) foreach (_ mustMatch { case Some(_) => true })
   }
 
-  "alignBoth" ! forAll{ (xs: List[Int], ys: List[Int]) =>
+  val alignBoth = forAll{ (xs: List[Int], ys: List[Int]) =>
     val xys = F.alignBoth(xs, ys)
     xys.size must_===(xs.size max ys.size)
     xys.takeWhile(_.isDefined).size must_===(xs.size min ys.size)
