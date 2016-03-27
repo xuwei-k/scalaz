f148ef9d51e45ea76f55d61764138fc650c1fb72
xuwei-k <6b656e6a69@gmail.com>
2016-03-27 16:50:13 +0900

    MaybeTests

--- tests/src/test/scala/scalaz/MaybeTests.scala
+++ tests/src/test/scala/scalaz/MaybeTests.scala
@@ -1,93 +1,96 @@
 package scalaz
 
-import org.scalacheck.Prop.forAll
 import Tags._
+import Property.forAll
 
-object MaybeTest extends SpecLite {
-  import scalaz.scalacheck.ScalazProperties._
-  import scalaz.scalacheck.ScalazArbitrary._
+object MaybeTest extends Scalaprops {
   import std.anyVal._
   import std.string._
   import syntax.equal._
 
   import Maybe._
 
-  checkAll("Maybe", order.laws[Maybe[Int]])
-  checkAll("Maybe @@ First", order.laws[FirstMaybe[Int]])
-  checkAll("Maybe @@ Last", order.laws[LastMaybe[Int]])
-  checkAll("Maybe @@ Min", order.laws[MinMaybe[Int]])
-  checkAll("Maybe @@ Max", order.laws[MaxMaybe[Int]])
+  private[this] implicit val stringGen: Gen[String] =
+    Tag.unsubst(Gen[String @@ GenTags.AlphaNum])
 
-  checkAll("Maybe", monoid.laws[Maybe[Int]])
-  checkAll("Maybe @@ First", monoid.laws[FirstMaybe[Int]])
-  checkAll("Maybe @@ Last", monoid.laws[LastMaybe[Int]])
-  checkAll("Maybe @@ Min", monoid.laws[MinMaybe[Int]])
-  checkAll("Maybe @@ Max", monoid.laws[MaxMaybe[Int]])
+  val order = laws.order.all[Maybe[Int]]
+  val firstOrder = laws.order.all[FirstMaybe[Int]]
+  val lastOrder = laws.order.all[LastMaybe[Int]]
+  val minOrder = laws.order.all[MinMaybe[Int]]
+  val maxOrder = laws.order.all[MaxMaybe[Int]]
 
-  checkAll("Maybe @@ First", monad.laws[FirstMaybe])
-  checkAll("Maybe @@ Last", monad.laws[LastMaybe])
-  checkAll("Maybe @@ Min", monad.laws[MinMaybe])
-  checkAll("Maybe @@ Max", monad.laws[MaxMaybe])
+  val monoid = laws.monoid.all[Maybe[Int]]
+  val firstMonoid = laws.monoid.all[FirstMaybe[Int]]
+  val lastMonoid = laws.monoid.all[LastMaybe[Int]]
+  val minMonoid = laws.monoid.all[MinMaybe[Int]]
+  val maxMonoid = laws.monoid.all[MaxMaybe[Int]]
 
-  checkAll(bindRec.laws[Maybe])
-  checkAll(monadPlus.strongLaws[Maybe])
-  checkAll(traverse.laws[Maybe])
-  checkAll(zip.laws[Maybe])
-  checkAll(isEmpty.laws[Maybe])
-  checkAll(cobind.laws[Maybe])
-  checkAll(align.laws[Maybe])
-  checkAll(equal.laws[Maybe[Int]])
+  val firstMonad = laws.monad.all[FirstMaybe]
+  val lastMonad = laws.monad.all[LastMaybe]
+  val minMonad = laws.monad.all[MinMaybe]
+  val maxMonad = laws.monad.all[MaxMaybe]
 
-  "Empty is less than anything else" ! forAll { x: Maybe[Int] => Order[Maybe[Int]].greaterThanOrEqual(x, Maybe.empty) }
+  val testLaws = Properties.list(
+    laws.bindRec.all[Maybe],
+    laws.monadPlusStrong.all[Maybe],
+    laws.traverse.all[Maybe],
+    laws.zip.all[Maybe],
+    laws.isEmpty.all[Maybe],
+    laws.cobind.all[Maybe],
+    laws.align.all[Maybe],
+    laws.equal.all[Maybe[Int]]
+  )
 
-  "Empty is ignored in Maybe[A]@@Min" ! forAll { x: Maybe[Int] =>
+  val `Empty is less than anything else` = forAll { x: Maybe[Int] => Order[Maybe[Int]].greaterThanOrEqual(x, Maybe.empty) }
+
+  val `Empty is ignored in Maybe[A]@@Min` = forAll { x: Maybe[Int] =>
     import syntax.monoid._
     (Min(x) |+| Min(empty)) must_=== Min(x)
   }
 
-  "Empty is ignored in Maybe[A]@@Max" ! forAll { x: Maybe[Int] =>
+  val `Empty is ignored in Maybe[A]@@Max` = forAll { x: Maybe[Int] =>
     import syntax.monoid._
     (Max(x) |+| Max(empty)) must_=== Max(x)
   }
 
-  "Preserved through Option" ! forAll { x: Maybe[Int] => std.option.toMaybe(x.toOption) === x }
+  val `Preserved through Option` = forAll { x: Maybe[Int] => std.option.toMaybe(x.toOption) === x }
 
-  "just toFailure is failure" ! forAll { (x: Int, s: String) => just(x).toFailure(s).isFailure }
+  val `just toFailure is failure` = forAll { (x: Int, s: String) => just(x).toFailure(s).isFailure }
 
-  "empty toFailure is success" ! forAll { s: String => empty.toFailure(s).isSuccess }
+  val `empty toFailure is success` = forAll { s: String => empty.toFailure(s).isSuccess }
 
-  "just toSuccess is success" ! forAll { (x: Int, s: String) => just(x).toSuccess(s).isSuccess }
+  val `just toSuccess is success` = forAll { (x: Int, s: String) => just(x).toSuccess(s).isSuccess }
 
-  "empty toSuccess is failure" ! forAll { s: String => empty.toSuccess(s).isFailure }
+  val `empty toSuccess is failure` = forAll { s: String => empty.toSuccess(s).isFailure }
 
-  "just toLeft is left" ! forAll { (x: Int, s: String) => just(x).toLeft(s).isLeft }
+  val `just toLeft is left` = forAll { (x: Int, s: String) => just(x).toLeft(s).isLeft }
 
-  "empty toLeft is right" ! forAll { s: String => empty.toLeft(s).isRight }
+  val `empty toLeft is right` = forAll { s: String => empty.toLeft(s).isRight }
 
-  "just toRight is right" ! forAll { (x: Int, s: String) => just(x).toRight(s).isRight }
+  val `just toRight is right` = forAll { (x: Int, s: String) => just(x).toRight(s).isRight }
 
-  "empty toRight is left" ! forAll { s: String => empty.toRight(s).isLeft }
+  val `empty toRight is left` = forAll { s: String => empty.toRight(s).isLeft }
 
-  "just isJust" ! forAll { x: Int => just(x).isJust }
+  val `just isJust` = forAll { x: Int => just(x).isJust }
 
-  "just isn't empty" ! forAll { x: Int => !just(x).isEmpty }
+  val `just isn't empty` = forAll { x: Int => !just(x).isEmpty }
 
-  "empty is empty" ! check(empty.isEmpty)
+  val `empty is empty` = forAll(empty.isEmpty)
 
-  "empty isn't just" ! check(!empty.isJust)
+  val `empty isn't just` = forAll(!empty.isJust)
 
-  "just to option is some" ! forAll { x: Int => just(x).toOption.isDefined }
+  val `just to option is some` = forAll { x: Int => just(x).toOption.isDefined }
 
-  "empty to option is none" ! check(empty.toOption.isEmpty)
+  val `empty to option is none` = forAll(empty.toOption.isEmpty)
 
-  "just orElse is just" ! forAll { (x: Int, m: Maybe[Int]) => just(x).orElse(m).isJust }
+  val `just orElse is just` = forAll { (x: Int, m: Maybe[Int]) => just(x).orElse(m).isJust }
 
-  "fromNullable(null) is Empty" ! check {
+  val `fromNullable(null) is Empty` = forAll {
     val s: String = null
     Maybe.fromNullable(s).isEmpty
   }
 
-  "fromNullable(notNull) is just" ! forAll { (s: String) => Maybe.fromNullable(s) must_=== just(s) }
+  val `fromNullable(notNull) is just` = forAll { (s: String) => Maybe.fromNullable(s) must_=== just(s) }
 
   object instances {
     def equal[A: Equal] = Equal[Maybe[A]]
