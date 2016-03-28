56f60522aec9973c08da42eda90cc6a08ef6125c
xuwei-k <6b656e6a69@gmail.com>
2016-03-28 19:29:36 +0900

    EphemeralStreamTest

--- tests/src/test/scala/scalaz/EphemeralStreamTest.scala
+++ tests/src/test/scala/scalaz/EphemeralStreamTest.scala
@@ -1,91 +1,91 @@
 package scalaz
 
-import scalaz.scalacheck.ScalazProperties._
-import scalaz.scalacheck.ScalazArbitrary._
 import std.AllInstances._
 import syntax.contravariant._
-import org.scalacheck.Prop.forAll
+import Property.forAll
 
-object EphemeralStreamTest extends SpecLite {
+object EphemeralStreamTest extends Scalaprops {
 
-  checkAll(equal.laws[EphemeralStream[Int]])
-  checkAll(bindRec.laws[EphemeralStream])
-  checkAll(monadPlus.strongLaws[EphemeralStream])
-  checkAll(isEmpty.laws[EphemeralStream])
-  checkAll(traverse.laws[EphemeralStream])
-  checkAll(zip.laws[EphemeralStream])
-  checkAll(align.laws[EphemeralStream])
-  checkAll(cobind.laws[EphemeralStream])
+  val testLaws = Properties.list(
+    laws.equal.all[EphemeralStream[Int]],
+    laws.bindRec.all[EphemeralStream],
+    laws.monadPlusStrong.all[EphemeralStream],
+    laws.isEmpty.all[EphemeralStream],
+    laws.traverse.all[EphemeralStream],
+    laws.zip.all[EphemeralStream],
+    laws.align.all[EphemeralStream],
+    laws.cobind.all[EphemeralStream]
+  )
 
   implicit def ephemeralStreamShow[A: Show]: Show[EphemeralStream[A]] =
     Show[List[A]].contramap(_.toList)
 
-  "reverse" ! forAll{ e: EphemeralStream[Int] =>
+  val reverse = forAll{ e: EphemeralStream[Int] =>
     e.reverse.toList must_===(e.toList.reverse)
     e.reverse.reverse must_===(e)
   }
 
-  "foldLeft" ! forAll{ xs: List[List[Int]] =>
+  val foldLeft = forAll{ xs: List[List[Int]] =>
     Foldable[EphemeralStream].foldLeft(EphemeralStream(xs: _*), List[Int]())(_ ::: _) must_===(xs.foldLeft(List[Int]())(_ ::: _))
   }
 
-  "unzip zip" ! forAll { xs: EphemeralStream[(Int, Int)] =>
+  val `unzip zip` = forAll { xs: EphemeralStream[(Int, Int)] =>
     val (firsts, seconds) = xs.unzip
     (firsts zip seconds) must_===(xs)
   }
 
-  "zip has right length" ! forAll {(xs: EphemeralStream[Int], ys: EphemeralStream[Int]) =>
+  val `zip has right length` = forAll {(xs: EphemeralStream[Int], ys: EphemeralStream[Int]) =>
     (xs zip ys).length must_===(xs.length min ys.length)
   }
 
-  "interleave has right length" ! forAll {(xs: EphemeralStream[Int], ys: EphemeralStream[Int]) =>
+  val `interleave has right length` = forAll {(xs: EphemeralStream[Int], ys: EphemeralStream[Int]) =>
     (xs interleave ys).length must_===(xs.length + ys.length)
   }
 
-  "take" ! forAll { (xs: Stream[Int], n: Int) =>
+  val take = forAll { (xs: Stream[Int], n: Int) =>
     EphemeralStream.fromStream(xs).take(n) must_===(EphemeralStream.fromStream(xs.take(n)))
   }
 
-  "take from infinite stream" in {
+  val `take from infinite stream` = forAll {
     val n = util.Random.nextInt(1000)
     EphemeralStream.iterate(0)(_ + 1).take(n) must_===(EphemeralStream.fromStream(Stream.iterate(0)(_ + 1).take(n)))
   }
 
-  "takeWhile" ! forAll { (xs: Stream[Int], n: Int) =>
+  val takeWhile = forAll { (xs: Stream[Int], n: Int) =>
     EphemeralStream.fromStream(xs).takeWhile(_ < n) must_===(EphemeralStream.fromStream(xs.takeWhile(_ < n)))
   }
 
-  "takeWhile from infinite stream" in {
+  val `takeWhile from infinite stream` = forAll {
     val n = util.Random.nextInt(1000)
     EphemeralStream.iterate(0)(_ + 1).takeWhile(_ < n) must_===(EphemeralStream.fromStream(Stream.iterate(0)(_ + 1).takeWhile(_ < n)))
   }
 
-  "index" ! forAll {(xs: EphemeralStream[Int], i: Int) =>
+  val index = forAll {(xs: EphemeralStream[Int], i: Int) =>
     Foldable[EphemeralStream].index(xs, i) must_===(xs.toList.lift.apply(i))
   }
 
-  "index infinite stream" in {
+  val `index infinite stream` = forAll {
     val i = util.Random.nextInt(1000)
     val xs = Stream from 0
     Foldable[EphemeralStream].index(EphemeralStream.fromStream(xs), i) must_===(xs.lift.apply(i))
   }
 
-  "inits" ! forAll { xs: EphemeralStream[Int] =>
+  val inits = forAll { xs: EphemeralStream[Int] =>
     import syntax.std.list._
     xs.inits.map(_.toList).toList must_===(xs.toList.initz)
   }
 
-  "tails" ! forAll { xs: EphemeralStream[Int] =>
+  val tails = forAll { xs: EphemeralStream[Int] =>
     import syntax.std.list._
     xs.tails.map(_.toList).toList must_===(xs.toList.tailz)
   }
 
-  "inits infinite stream" in {
+  val `inits infinite stream` = forAll {
     EphemeralStream.iterate(0)(_ + 1).inits
     ()
   }
 
-  "tails infinite stream" in {
+  val `tails infinite stream` = forAll {
     val n = util.Random.nextInt(1000)
     EphemeralStream.iterate(0)(_ + 1).tails
       .map(t => Foldable[EphemeralStream].toStream(t.take(n)))
@@ -94,17 +94,17 @@ object EphemeralStreamTest extends SpecLite {
     )
   }
 
-  "foldMap evaluates lazily" in {
+  val `foldMap evaluates lazily` = forAll {
     val infiniteStream = EphemeralStream.iterate(false)(identity)
     Foldable[EphemeralStream].foldMap(infiniteStream)(identity)(booleanInstance.conjunction) must_===(false)
   }
 
-  "foldRight evaluates lazily" in {
+  val `foldRight evaluates lazily` = forAll {
     val infiniteStream = EphemeralStream.iterate(true)(identity)
     Foldable[EphemeralStream].foldRight(infiniteStream, true)(_ || _) must_===(true)
   }
 
-  "zipL" in {
+  val zipL = forAll {
     val size = 100
     val infinite = EphemeralStream.iterate(0)(_ + 1)
     val finite = EphemeralStream.range(0, size)
@@ -116,11 +116,11 @@ object EphemeralStreamTest extends SpecLite {
     F.zipL(infinite, finite).takeWhile(_._2.isDefined).length must_===(size)
   }
 
-  "zipWithIndex" ! forAll { (xs: Stream[Int], n: Int) =>
+  val zipWithIndex = forAll { (xs: Stream[Int], n: Int) =>
     EphemeralStream.fromStream(xs).take(n).zipWithIndex must_===(EphemeralStream.fromStream(xs.take(n).zipWithIndex))
   }
 
-  "zipWithIndex from infinite stream" in {
+  val `zipWithIndex from infinite stream` = forAll {
     val n = util.Random.nextInt(1000)
     EphemeralStream.iterate(0)(_ + 1).zipWithIndex.take(n) must_===(
       EphemeralStream.fromStream(Stream.iterate(0)(_ + 1).zipWithIndex.take(n))
